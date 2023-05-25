dcbc41d9f1fa29d52ad72ee2765c11ee1ee03234
AnnaSaurina <the_happy_ann@mail.ru>
2023-05-25 17:55:30 +0300

    Saved

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -3,37 +3,26 @@ let urlImage = document.getElementById('urlElement');
 let comment = document.getElementById('commentElement');
 let button = document.querySelector('.buttonElement');
 let chat = document.querySelector('.chat');
-let checkName = document.getElementById('checkboxName');
 
 function checkedWord(word) {
-    return (`${word.slice(0, 1).toUpperCase()}${word.slice(1).toLowerCase()}`);
+    return (word.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' '));
 };
 
-
-
 button.addEventListener('click', () => {
-    console.log(userName.value);
 
     let urlAvatar = urlImage.value;
-    console.log(urlImage.value);
-
     let user = userName.value;
-    let fullName = user.split(' ');
-    let surname = checkedWord(fullName[0]);
-    let yourName = checkedWord(fullName[1]); 
-    let patronymik = checkedWord(fullName[2]);
-    let finalFullName = `${surname} ${yourName} ${patronymik}`;
-    let now = new Date();
-    console.log(now);
+    let finalFullName = checkedWord(user);
+    let commentChat = comment.value;
+    let chatSpam = commentChat.replace(/xxx/gi, '***').replace(/viagra/gi, '***');
 
     chat.innerHTML = `
         <div id="message" class="message">
                     <p>Чат</p>
                     <img src=${urlAvatar}
                     alt="Аватар" class="avatar" />
-                    <p class="userName">${now}</p> 
                     <p class="userName">${finalFullName}</p> 
-                    <p class="text">${comment.value}</p>
+                    <p class="text">${chatSpam}</p>
                 </div>
                 `;
 });
